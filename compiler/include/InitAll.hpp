b6518fb80e140f8da2dc4ae42bab2eadf7c315a5
Abhishek-TyRnT <kulkarnia168@gmail.com>
2024-10-07 20:04:48 +0530

    registered Dialects and passes

---
+++ compiler/include/InitAll.hpp
@@ -0,0 +1,24 @@
+#ifndef VLLM_GRAPH_INITALl_H
+#define VLLM_GRAPH_INITALl_H
+
+#include "mlir/IR/Dialect.h"
+
+namespace mlir {
+namespace vllm_graph{
+
+// Registers all dialects that this project produces and any dependencies.
+void registerAllDialects(mlir::DialectRegistry &registry);
+
+// // Registers all necessary dialect extensions for this project
+// void registerAllExtensions(mlir::DialectRegistry &registry);
+
+// Registers dialects that may be needed to parse torch-mlir inputs and
+// test cases.
+void registerOptionalInputDialects(mlir::DialectRegistry &registry);
+
+void registerAllPasses();
+
+} //namespace vllm_graph
+} // mlir
+
+#endif // VLLM_GRAPH_INITALl_H
---
+++ compiler/lib/InitAll.cpp
@@ -0,0 +1,20 @@
+
+#include "InitAll.hpp"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/Dialect.h"
+#include "vllm_graph/Conversion/Passes.h"
+#include "vllm_graph/Conversion/TorchTovLLMGraph.hpp"
+#include "vllm_graph/Dialect/IR/vLLMGraphDialect.hpp"
+
+
+void mlir::vllm_graph::registerAllDialects(mlir::DialectRegistry &registry)
+{
+    registry.insert<mlir::func::FuncDialect>();
+    registry.insert<mlir::vllm_graph::vLLMGraphIRDialect>();
+
+}
+
+void mlir::vllm_graph::registerAllPasses() {
+    mlir::vllm_graph::registerConversionPasses();
+}
+

1d6ea9172879ea47a3e63b7503ce01cc3bac4579
Abhishek-TyRnT <kulkarnia168@gmail.com>
2024-10-17 23:06:18 +0530

    added vLLMGraph types

--- compiler/lib/vllm_graph/Dialect/IR/vLLMGraphDialect.cpp
+++ compiler/lib/vllm_graph/Dialect/IR/vLLMGraphDialect.cpp
@@ -2,15 +2,17 @@
 #include "vllm_graph/Dialect/IR/vLLMGraphDialect.hpp"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/Types.h"
 #include "mlir/IR/DialectImplementation.h"
 #include "mlir/IR/IRMapping.h"
 #include "mlir/Transforms/InliningUtils.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "vllm_graph/Dialect/IR/vLLMGraphOps.hpp"
+#include "vllm_graph/Dialect/IR/vLLMGraphTypes.hpp"
 
-//#include "torch-mlir/Dialect/Torch/IR/TorchTypes.h"
-#include "torch-mlir/Dialect/Torch/IR/TorchOps.h"
-#include "torch-mlir/Dialect/Torch/IR/TorchDialect.h"
-#include "torch-mlir/Dialect/Torch/IR/TorchTypes.h.inc"
+// #include "torch-mlir/Dialect/Torch/IR/TorchTypes.h"
+// #include "torch-mlir/Dialect/Torch/IR/TorchOps.h"
+//#include "torch-mlir/Dialect/Torch/IR/TorchDialect.h"
 
 // #include "torch-mlir/Dialect/Torch/IR/TorchDialect.cpp.inc"
 // #include "torch-mlir/Dialect/Torch/IR/TorchOps.cpp.inc"
@@ -21,22 +23,29 @@
 
 using namespace mlir;
 using namespace mlir::vllm_graph;
-using namespace mlir::torch;
-using namespace mlir::torch::Torch;
+// using namespace mlir::torch;
+// using namespace mlir::torch::Torch;
 #include "vllm_graph/Dialect/IR/vLLMGraphIRDialect.cpp.inc"
 
-// #define GET_TYPEDEF_CLASSES
-// #include "torch-mlir/Dialect/Torch/IR/TorchTypes.h.inc"
-
-// #include "torch-mlir/Dialect/Torch/IR/TorchTypes.cpp.inc"
+#define GET_TYPEDEF_CLASSES
+#include "vllm_graph/Dialect/IR/vLLMGraphTypes.cpp.inc"
 
 
 Type vllm_graph::parsevLLMGraphDialectType(AsmParser &parser){
-    return Torch::parseTorchDialectType(parser);
+    SMLoc typeLoc = parser.getCurrentLocation();
+    StringRef mnemonic;
+    Type genType;
+    auto parseResult = generatedTypeParser(parser, &mnemonic, genType);
+    if(parseResult.has_value())
+        return genType;
+    parser.emitError(typeLoc) << "unknown  type `" << mnemonic << "` in dialect `"
+                                << vLLMGraphIRDialect::getDialectNamespace() << "`";
+    return {};
 }
 
 void vllm_graph::printvLLMGraphDialectType(Type type, AsmPrinter &printer) {
-    Torch::printTorchDialectType(type, printer);
+    if (succeeded(generatedTypePrinter(type, printer)))
+        return;
 }
 
 
@@ -60,7 +69,7 @@ void vLLMGraphIRDialect::initialize() {
         >();
     addTypes<
 #define GET_TYPEDEF_LIST
-#include "torch-mlir/Dialect/Torch/IR/TorchTypes.cpp.inc"
+#include "vllm_graph/Dialect/IR/vLLMGraphTypes.cpp.inc"
       >();
     
 }
@@ -68,36 +77,132 @@ void vLLMGraphIRDialect::initialize() {
 Operation *vLLMGraphIRDialect::materializeConstant(OpBuilder &builder,
                                              Attribute value, Type type,
                                              Location loc) {
-    if (auto integerType = dyn_cast<Torch::IntType>(type))
-        return builder.create<Torch::ConstantIntOp>(loc, cast<IntegerAttr>(value));
+    if (auto integerType = dyn_cast<vllm_graph::IntType>(type))
+        return builder.create<arith::ConstantIntOp>(loc, cast<IntegerAttr>(value).getValue().getSExtValue(), builder.getIntegerType(32));
 
-    if (auto floatType = dyn_cast<Torch::FloatType>(type))
-        return builder.create<Torch::ConstantFloatOp>(loc, cast<FloatAttr>(value));
+    if (auto floatType = dyn_cast<vllm_graph::FloatType>(type))
+        return builder.create<arith::ConstantFloatOp>(loc, cast<FloatAttr>(value).getValue(), builder.getF32Type());
 
-    if (auto numberType = dyn_cast<Torch::NumberType>(type)) {
-        if (auto floatValue = dyn_cast<mlir::FloatAttr>(value)) {
-        return builder.create<Torch::ConstantNumberOp>(loc, floatValue);
-        } else if (auto intValue = dyn_cast<mlir::IntegerAttr>(value)) {
-        return builder.create<Torch::ConstantNumberOp>(loc, intValue);
-        }
+
+    // TODO: Add number type
+    // if (auto numberType = dyn_cast<vllm_graph::NumberType>(type)) {
+    //     if (auto floatValue = dyn_cast<mlir::FloatAttr>(value)) {
+    //     return builder.create<vllm_graph::ConstantNumberOp>(loc, floatValue);
+    //     } else if (auto intValue = dyn_cast<mlir::IntegerAttr>(value)) {
+    //     return builder.create<vllm_graph::ConstantNumberOp>(loc, intValue);
+    //     }
+    // }
+
+    if (isa<vllm_graph::BoolType>(type)) {
+        return builder.create<arith::ConstantIntOp>(loc, cast<IntegerAttr>(value).getValue().getZExtValue(), builder.getI1Type());
     }
 
-    if (isa<Torch::BoolType>(type)) {
-        return builder.create<Torch::ConstantBoolOp>(loc, cast<IntegerAttr>(value));
-    }
+    // if (isa<vllm_graph::NoneType>(type))
+    //     return builder.create<arith::ConstantOp>(loc, mlir::NoneType::get(builder.getContext()));
+;
 
-    if (isa<Torch::NoneType>(type))
-        return builder.create<ConstantNoneOp>(loc);
+    // if (auto stringAttr = dyn_cast<StringAttr>(value))
+    //     return builder.create<ConstantStrOp>(loc, stringAttr);
 
-    if (auto stringAttr = dyn_cast<StringAttr>(value))
-        return builder.create<ConstantStrOp>(loc, stringAttr);
-
-    if (auto elementsAttr = dyn_cast<ElementsAttr>(value)) {
-        // Only !torch.vtensor can be constant folded. !torch.tensor has
-        // non-trivial aliasing semantics which prevent deduplicating it.
-        assert(isa<ValueTensorType>(type) && "should be a vtensor type!");
-        return builder.create<ValueTensorLiteralOp>(loc, elementsAttr);
-    }
+    // if (auto elementsAttr = dyn_cast<vllm_graph::ElementsAttr>(value)) {
+    //     // Only !torch.vtensor can be constant folded. !torch.tensor has
+    //     // non-trivial aliasing semantics which prevent deduplicating it.
+    //     assert(isa<vllm_graph::ValueTensorType>(type) && "should be a vtensor type!");
+    //     return builder.create<vllm_graph::ValueTensorLiteralOp>(loc, elementsAttr);
+    // }
 
     return nullptr;
+}
+
+void OptionalType::print(AsmPrinter &printer) const {
+  printer << "<";
+  // Print the contained type without the `!torch.` prefix.
+  printvLLMGraphDialectType(getImpl()->containedType, printer);
+  printer << ">";
+}
+
+void ListType::print(AsmPrinter &printer) const {
+  printer << "<";
+  // Print the contained type without the `!torch.` prefix.
+  printvLLMGraphDialectType(getImpl()->containedType, printer);
+  printer << ">";
+}
+
+Type OptionalType::parse(AsmParser &odsParser) {
+  if (odsParser.parseLess())
+    return Type();
+
+  // Parse the contained type, but forward directly to our internal parsing
+  // of `torch` dialect types, so that we can parse nested types without
+  // the `!torch.` prefix.
+  Type containedType = parsevLLMGraphDialectType(odsParser);
+  if (!containedType)
+    return Type();
+  if (odsParser.parseGreater())
+    return Type();
+  return get(odsParser.getContext(), containedType);
+}
+
+Type ListType::parse(AsmParser &odsParser) {
+  if (odsParser.parseLess())
+    return Type();
+
+  // Parse the contained type, but forward directly to our internal parsing
+  // of `torch` dialect types, so that we can parse nested types without
+  // the `!torch.` prefix.
+  Type containedType = parsevLLMGraphDialectType(odsParser);
+  if (!containedType)
+    return Type();
+  if (odsParser.parseGreater())
+    return Type();
+  return get(odsParser.getContext(), containedType);
+}
+
+//===----------------------------------------------------------------------===//
+// DictType
+//===----------------------------------------------------------------------===//
+
+void DictType::print(AsmPrinter &printer) const {
+  printer << "<";
+  printvLLMGraphDialectType(getImpl()->keyType, printer);
+  printer << ", ";
+  printvLLMGraphDialectType(getImpl()->valueType, printer);
+  printer << ">";
+}
+
+Type DictType::parse(AsmParser &odsParser) {
+  if (odsParser.parseLess())
+    return Type();
+  Type keyType = parsevLLMGraphDialectType(odsParser);
+  if (!keyType)
+    return Type();
+  if (odsParser.parseComma())
+    return Type();
+  Type valueType = parsevLLMGraphDialectType(odsParser);
+  if (!valueType)
+    return Type();
+  if (odsParser.parseGreater())
+    return Type();
+  return get(odsParser.getContext(), keyType, valueType);
+}
+
+//===----------------------------------------------------------------------===//
+// NnModuleType
+//===----------------------------------------------------------------------===//
+
+void NnModuleType::print(AsmPrinter &printer) const {
+  printer << "<\"";
+  llvm::printEscapedString(getImpl()->className, printer.getStream());
+  printer << "\">";
+}
+
+Type NnModuleType::parse(AsmParser &odsParser) {
+  if (odsParser.parseLess())
+    return Type();
+  std::string className;
+  if (odsParser.parseOptionalString(&className))
+    return Type();
+  if (odsParser.parseGreater())
+    return Type();
+  return get(odsParser.getContext(), className);
 }

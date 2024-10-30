42d8b6facf3a8c27aa281dce015eb8d0c81cbdd6
Abhishek-TyRnT <kulkarnia168@gmail.com>
2024-10-30 08:13:20 +0530

    Added castOps

--- compiler/lib/vllm_graph/Dialect/Transform/TransformPasses.cpp
+++ compiler/lib/vllm_graph/Dialect/Transform/TransformPasses.cpp
@@ -15,7 +15,7 @@ using namespace mlir::vllm_graph;
 
 
 
-func::FuncOp replaceFuncDtypes(func::FuncOp &op)
+void replaceFuncDtypes(func::FuncOp &op)
 {
     mlir::FunctionType oldFuncType = op.getFunctionType();
     MLIRContext *context = op.getContext();
@@ -62,7 +62,7 @@ func::FuncOp replaceFuncDtypes(func::FuncOp &op)
     // Create the new function type with the updated argument types
     // mlir::FunctionType newFuncType = builder.getFunctionType(newArgTypes, newResultTypes);
     // auto newFuncOp = builder.create<mlir::func::FuncOp>(op.getLoc(), op.getName(), newFuncType);
-    mlir::FunctionType newFuncType = builder.getFunctionType(newArgTypes, oldResultTypes);
+    mlir::FunctionType newFuncType = builder.getFunctionType(newArgTypes, newResultTypes);
     // auto *entryBlock = &newFuncOp.getBody().front();
     op.setType(newFuncType);
     mlir::Value arg;
@@ -80,40 +80,27 @@ func::FuncOp replaceFuncDtypes(func::FuncOp &op)
     auto castOp = builder.create<vllm_graph::CastOp>(loc, oldArgTypes[0], arg);
     mlir::Value castOpResult = castOp.getResult();
     nextOp.setOperand(0, castOpResult);
-    //entryBlock.push_front(&cast<Operation>(castOp));
 
-    llvm::outs() << entryBlock <<"\n";
-      // Now we need to replace the old arguments with the new arguments in the body
-    //   mlir::Block &entryBlock = newFuncOp.front();
-    //   std::cout << __LINE__ << " " << __FILE__ << std::endl;
-    //   for (unsigned i = 0, e = entryBlock.getNumArguments(); i < e; ++i) {
-    //     std::cout << __LINE__ << " " << __FILE__ << std::endl;
-    //     mlir::Value oldArg = entryBlock.getArgument(i);
-    //     mlir::Type newArgType = newArgTypes[i];
-
-    //     // Create a new argument with the new type
-    //     mlir::Value newArg = entryBlock.addArgument(newArgType, newFuncOp.getLoc());
-
-    //     // Replace old argument uses with the new one
-    //     oldArg.replaceAllUsesWith(newArg);
-
-    //     // Optionally: Erase the old argument
-    //     entryBlock.eraseArgument(i);
-    //     std::cout << __LINE__ << " " << __FILE__ << std::endl;
-    //   }
-    
-    // std::cout << __LINE__ << " " << __FILE__ << std::endl;
-    // // Erase the old function
-    // op->replaceAllUsesWith(newFuncOp);
-    // op->erase();
-    // llvm::outs() << newFuncOp << "\n";
-    return op;
+    for(mlir::Operation &currOp : entryBlock)
+    {
+        if(mlir::isa<func::ReturnOp>(currOp))
+        {
+            auto returnOp = mlir::cast<func::ReturnOp>(currOp);
+            std::cout << __LINE__ << std::endl;
+            mlir::Operation *returnOperation = returnOp.getOperation();
+            mlir::Value returnValue = returnOperation->getOperand(0);
+            builder.setInsertionPoint(returnOp);
+            mlir::Location returnloc = returnOp.getLoc();
+            auto returnCastOp = builder.create<vllm_graph::CastOp>(returnloc, newResultTypes[0], returnValue);
+            returnOperation->setOperand(0, returnCastOp);
+        }
+    }
 }
 LogicalResult convertFuncOp(ModuleOp &moduleOp){
     for (auto &op : moduleOp.getBody()->getOperations()) {
         if(auto funcOp = dyn_cast<func::FuncOp>(op))
         {
-            func::FuncOp newFunc = replaceFuncDtypes(funcOp);
+            replaceFuncDtypes(funcOp);
             //moduleOp.push_back(newFunc);
             return mlir::success();
         }
@@ -122,107 +109,7 @@ LogicalResult convertFuncOp(ModuleOp &moduleOp){
     return mlir::failure();
 }
 
-// class ConvertFuncOp : public OpConversionPattern<mlir::func::FuncOp> {
-// public:
-//   using OpConversionPattern<mlir::func::FuncOp>::OpConversionPattern;
-//   using OpAdaptor = typename mlir::func::FuncOp::Adaptor;
-//   LogicalResult
-//   matchAndRewrite(mlir::func::FuncOp op, OpAdaptor adaptor,
-//                   ConversionPatternRewriter &rewriter) const override {
-    
-//     mlir::FunctionType oldFuncType = op.getFunctionType();
-//     llvm::outs() << op << "\n";
 
-//     // Get current argument and result types
-//     llvm::ArrayRef<mlir::Type> oldArgTypes = oldFuncType.getInputs();
-//     llvm::ArrayRef<mlir::Type> oldResultTypes = oldFuncType.getResults();
-
-    
-//     llvm::SmallVector<mlir::Type, 4> newArgTypes;
-//     MLIRContext *context = op.getContext();
-
-//     auto typeConverter_fn = [context](Type type) {
-//     Type opType;
-//     if(auto torchvTensor = cast<mlir::torch::Torch::ValueTensorType>(type)){
-//         vllm_graph::ValueTensorType vLLMvTensor;
-//         vLLMvTensor = vLLMvTensor.get(context, 
-//                         torchvTensor.getOptionalSizes(), 
-//                         torchvTensor.getOptionalDtype(), 
-//                         torchvTensor.getOptionalSparsity());
-//         opType = cast<Type>(vLLMvTensor);
-//         return opType;
-//         }
-//     else
-//         return opType;
-//     };
-//     for (mlir::Type argType : oldArgTypes) {
-//         if(auto newargType = typeConverter_fn(argType))
-//             newArgTypes.push_back(newargType);
-//         else
-//             newArgTypes.push_back(argType);
-//     }
-
-//     llvm::SmallVector<mlir::Type, 4> newResultTypes;
-//     for (mlir::Type argType : oldResultTypes) {
-//         if(auto newargType = typeConverter_fn(argType))
-//             newResultTypes.push_back(newargType);
-//         else
-//             newResultTypes.push_back(argType);  
-//     }
-
-
-//     // Create the new function type with the updated argument types
-//     mlir::FunctionType newFuncType = rewriter.getFunctionType(newArgTypes, newResultTypes);
-//     auto newFuncOp = rewriter.create<mlir::func::FuncOp>(op.getLoc(), op.getName(), newFuncType);
-
-//     if (!op.getBody().empty()) {
-//       rewriter.inlineRegionBefore(op.getBody(), newFuncOp.getBody(), newFuncOp.end());
-
-//       // Now we need to replace the old arguments with the new arguments in the body
-//       mlir::Block &entryBlock = newFuncOp.front();
-//       for (unsigned i = 0, e = entryBlock.getNumArguments(); i < e; ++i) {
-//         mlir::Value oldArg = entryBlock.getArgument(i);
-//         mlir::Type newArgType = newArgTypes[i];
-
-//         // Create a new argument with the new type
-//         mlir::Value newArg = entryBlock.addArgument(newArgType, newFuncOp.getLoc());
-
-//         // Replace old argument uses with the new one
-//         oldArg.replaceAllUsesWith(newArg);
-
-//         // Optionally: Erase the old argument
-//         entryBlock.eraseArgument(i);
-//       }
-//     }
-
-//     // Erase the old function
-//     rewriter.eraseOp(op);
-
-//     // Set the new type on the function
-//     // rewriter.updateRootInPlace(op, [&]() {
-//     //   op.setType(newFuncType);
-//     // });
-
-//     // // If necessary, adjust the function body for the new argument types
-//     // mlir::Block &entryBlock = funcOp.front();
-//     // for (unsigned i = 0, e = entryBlock.getNumArguments(); i < e; ++i) {
-//     //   mlir::Value oldArg = entryBlock.getArgument(i);
-//     //   mlir::Type newArgType = newArgTypes[i];
-
-//     //   // Create a new argument with the new type
-//     //   mlir::Value newArg = entryBlock.addArgument(newArgType, funcOp.getLoc());
-
-//     //   // Replace old argument uses with the new one
-//     //   oldArg.replaceAllUsesWith(newArg);
-
-//     //   // Optionally: Erase the old argument
-//     //   entryBlock.eraseArgument(i);
-//     // }
-
-//     return mlir::success();
-
-//     }
-// };
 
 namespace {
 class ConvertGlobalTorchGraph : public ConvertGlobalFunctionPassBase<ConvertGlobalTorchGraph> {

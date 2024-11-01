62d910765c4a7dd34bae508ba1e14a02ce54982b
Abhishek-TyRnT <58800592+Abhishek-TyRnT@users.noreply.github.com>
2024-11-01 18:57:00 +0530

    Added AddTensorOp (#4)

--- compiler/lib/vllm_graph/Conversion/TorchTovLLMGraph/TorchTovLLMGraph.cpp
+++ compiler/lib/vllm_graph/Conversion/TorchTovLLMGraph/TorchTovLLMGraph.cpp
@@ -10,6 +10,7 @@
 #include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/IR/Matchers.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/ADT/TypeSwitch.h"
 #include <iostream>
 #include <numeric>
@@ -19,6 +20,24 @@ using namespace mlir;
 using namespace mlir::vllm_graph;
 using namespace mlir::torch::Torch;
 
+
+Type convertTorchvTypeTovLLMvType(Type type, MLIRContext *context){
+
+    auto torchvTensor = cast<mlir::torch::Torch::ValueTensorType>(type);
+    if(torchvTensor){
+        Type opType;
+        vllm_graph::ValueTensorType vLLMvTensor;
+        vLLMvTensor = vLLMvTensor.get(context, 
+                        torchvTensor.getOptionalSizes(), 
+                        torchvTensor.getOptionalDtype(), 
+                        torchvTensor.getOptionalSparsity());
+        opType = cast<Type>(vLLMvTensor);
+        return opType;
+    }
+
+    else
+        return torchvTensor;
+}
 
 namespace {
 
@@ -56,22 +75,61 @@ LogicalResult ConvertAtenOp<mlir::torch::Torch::AtenReluOp>::matchAndRewrite(
                                        "Only Tensor types supported in vllm_graph");
     }
 
-    Type opType;
-    if(auto torchvTensor = cast<mlir::torch::Torch::ValueTensorType>(selfTy)){
-        vllm_graph::ValueTensorType vLLMvTensor;
-        vLLMvTensor = vLLMvTensor.get(context, 
-                        torchvTensor.getOptionalSizes(), 
-                        torchvTensor.getOptionalDtype(), 
-                        torchvTensor.getOptionalSparsity());
-        opType = cast<Type>(vLLMvTensor);
-    }
+    Type opType = convertTorchvTypeTovLLMvType(selfTy, context);
 
     self.setType(opType);
 
-    Value new_op = rewriter.replaceOpWithNewOp<vllm_graph::ReluOp>(op, opType, self);
+    rewriter.replaceOpWithNewOp<vllm_graph::ReluOp>(op, opType, self);
     return success();
 
-    }
+}
+
+template <>
+LogicalResult ConvertAtenOp<mlir::torch::Torch::ConstantIntOp>::matchAndRewrite(
+    mlir::torch::Torch::ConstantIntOp op, OpAdaptor adaptor,
+    ConversionPatternRewriter &rewriter) const {
+
+    int64_t constant = op.getValue();
+    Type intType = rewriter.getIntegerType(32);
+    Value arithOp = rewriter.replaceOpWithNewOp<arith::ConstantIntOp>(op, constant, intType);
+    return mlir::success();
+
+}
+
+template <>
+LogicalResult ConvertAtenOp<mlir::torch::Torch::AtenAddTensorOp>::matchAndRewrite(
+    mlir::torch::Torch::AtenAddTensorOp op, OpAdaptor adaptor,
+    ConversionPatternRewriter &rewriter) const {
+    
+    Value Tensor1 = op.getOperand(0);
+    Value Tensor2 = op.getOperand(1);
+    Value Alpha = op.getOperand(2);
+
+    //converting from torch.int or torch.float to simply int32, float32 
+    if(isa<mlir::torch::Torch::IntType>(Alpha.getType()))
+        Alpha.setType(rewriter.getIntegerType(32));
+    
+    else if(isa<mlir::torch::Torch::FloatType>(Alpha.getType()))
+        Alpha.setType(rewriter.getF32Type());
+    else
+        assert(false && "Alpha must be integer or float");
+
+    Value result = op.getResult();
+
+    MLIRContext *context = op.getContext();
+    assert(Tensor1.getType() == Tensor2.getType() && "The dtypes of the tensors1 must match");
+
+    Type Input1Type = convertTorchvTypeTovLLMvType(Tensor1.getType(), context);
+    Type Input2Type = convertTorchvTypeTovLLMvType(Tensor2.getType(), context);
+    Type resultType = convertTorchvTypeTovLLMvType(result.getType(), context);
+
+    Tensor1.setType(Input1Type);
+    Tensor2.setType(Input2Type);
+
+    rewriter.replaceOpWithNewOp<vllm_graph::AddOp>(op, resultType, Tensor1, Tensor2, Alpha);
+    return mlir::success();
+    
+}
 
 template <>
 LogicalResult EraseOp<mlir::vllm_graph::CastOp>::matchAndRewrite(
@@ -83,9 +141,7 @@ LogicalResult EraseOp<mlir::vllm_graph::CastOp>::matchAndRewrite(
     mlir::torch::Torch::ValueTensorType operandType = mlir::cast<mlir::torch::Torch::ValueTensorType>(operand.getType());
 
     /*If the op is last op before return Op its operand type must be changed else might change 
-    type of the func return
-    */
-
+    type of the func return */
     if(operandType){
         MLIRContext *context = op.getContext();
         vllm_graph::ValueTensorType vLLMvTensor;
@@ -128,6 +184,14 @@ public:
         patterns.add<ConvertAtenOp<mlir::torch::Torch::AtenReluOp>>(typeConverter,        
                                                          context);
         
+        target.addIllegalOp<mlir::torch::Torch::ConstantIntOp>();
+        patterns.add<ConvertAtenOp<mlir::torch::Torch::ConstantIntOp>>(typeConverter,        
+                                                         context);
+
+        target.addIllegalOp<mlir::torch::Torch::AtenAddTensorOp>();
+        patterns.add<ConvertAtenOp<mlir::torch::Torch::AtenAddTensorOp>>(typeConverter,        
+                                                         context);
+
         target.addIllegalOp<mlir::vllm_graph::CastOp>();
         patterns.add<EraseOp<mlir::vllm_graph::CastOp>>(typeConverter,        
                                                          context);

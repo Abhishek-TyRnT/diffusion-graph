97a55292dd0a2e437e9d92d7d75f3e84ce6da385
Abhishek-TyRnT <kulkarnia168@gmail.com>
2024-11-10 21:09:58 +0530

    added vllm-graph.cpp and refactored the code

---
+++ compiler/tools/vllm-graph-opt/vllm-graph-opt.cpp
@@ -0,0 +1,17 @@
+
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+#include "mlir/Transforms/Passes.h"
+#include "InitAll.hpp"
+
+using namespace mlir;
+
+int main(int argc, char **argv) {
+    mlir::vllm_graph::registerAllPasses();
+
+    DialectRegistry registry;
+    mlir::vllm_graph::registerAllDialects(registry);
+    return mlir::asMainReturnCode(mlir::MlirOptMain(
+      argc, argv, "MLIR modular optimizer driver\n", registry));
+    return 0;
+
+}
---
+++ compiler/tools/vllm-graph/vllm-graph.cpp
@@ -0,0 +1,15 @@
+#include "Interface.hpp"
+
+using namespace mlir;
+
+int main(int argc, char **argv) {
+
+    if(argv[1] == ""){
+        llvm::errs() << "Input File not provided" << "\n";
+        exit(-1);
+    }
+    mlir::vllm_graph::vLLMGraph graph;
+    graph.convert(argv[1]);
+    return 0;
+
+}

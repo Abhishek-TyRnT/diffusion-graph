e6b49822e93e98bfc560cd7f0c3f684794a8e440
Abhishek-TyRnT <kulkarnia168@gmail.com>
2024-11-26 20:18:10 +0530

    eliminated needless code

--- compiler/python/vLLMGraphBindings.cpp
+++ compiler/python/vLLMGraphBindings.cpp
@@ -20,36 +20,6 @@
 namespace py = pybind11;
 using namespace mlir::vllm_graph;
 
-// using NestedValueType = std::variant<std::string, std::vector<int64_t>>;
-// using ValueType = std::variant<std::string, 
-//                     std::unordered_map<std::string, NestedValueType>>;
-
-
-static py::object getMlirIrClass(const char* className) {
-  return py::module::import(MAKE_MLIR_PYTHON_QUALNAME("ir")).attr(className);
-}
-
-static py::object castMlirModuleToPythonObject(MlirModule *module) {
-  auto moduleClass = getMlirIrClass("Module");
-  auto moduleCapsule =
-      py::reinterpret_steal<py::object>(mlirPythonModuleToCapsule(*module));
-  py::object obj = moduleClass.attr(MLIR_PYTHON_CAPI_FACTORY_ATTR)(moduleCapsule);
-  return obj;
-}
-
-// static MlirModule castPythonObjectToMlirContext(py::object& moduleobj) {
-//   assert(!moduleobj.is_none() && "context cannot be None");
-//   auto contextCapsule = moduleobj.attr(MLIR_PYTHON_CAPI_PTR_ATTR);
-  
-//   MlirModule context = mlirPythonCapsuleToModule(contextCapsule.ptr());
-//   llvm::outs() << *unwrap(context) << "\n";
-//   if (mlirModuleIsNull(context)) {
-//     // An error will have already been set by the above.
-//     throw py::error_already_set();
-//   }
-//   return context;
-// }
-
 class vLLMGraph : public vLLMGraphBase{
 
 private:
@@ -61,11 +31,6 @@ std::unordered_map<std::string, ValueType> compile(std::string IRFile){
     convertor.build(moduleRef);
     
     return convertor.getGraph();
-    // module = moduleRef.get();
-    // MlirModule moduleC = wrap(module);
-    // moduleC_ptr = &moduleC;
-    // moduleObj = castMlirModuleToPythonObject(moduleC);
-    //castPythonObjectToMlirContext(moduleObj);
 }
 
 };
@@ -75,8 +40,4 @@ PYBIND11_MODULE(graph_compiler, m){
     py::class_<vLLMGraph>(m, "vLLMGraph")
         .def(py::init())
         .def("convert", &vLLMGraph::compile);
-    // m.doc() = "Python bindings for vllm graph compiler";
-
-    // m.def("registerDialect", &registervLLMGraphDialect);
-    // m.def("registerPasses", &registervLLMGraphPasses);
 }

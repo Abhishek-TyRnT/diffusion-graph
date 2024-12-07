32ca1477c439d157fd0f05c663aef95b4a09e9cd
Abhishek-TyRnT <kulkarnia168@gmail.com>
2024-12-07 20:29:19 +0530

    created a python interface

---
+++ compiler/python/compiler.py
@@ -0,0 +1,15 @@
+from .graph_compiler import vLLMGraph
+import torch
+from torch.nn import Module
+from torch_mlir import torchscript
+
+
+class GraphCompiler:
+    def __init__(self, weight_path: str, debug: bool = False):
+        self.compiler = vLLMGraph(weight_path)
+        self.debug = debug
+
+    def compile(self, model: Module, inputs: list[torch.Tensor]) -> dict:
+        torchIR = torchscript.compile(model, inputs, output_type="torch")
+        IRDict = self.compiler.compile(str(torchIR))
+        return IRDict
